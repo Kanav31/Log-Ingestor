b5ce8c554b5c1f39cbd0606a679cf967fe845949
Kanav Agarwal <agarwalkanav3108@gmail.com>
2023-11-18 13:21:57 +0530

    new commit

--- server/controllers/logController.js
+++ server/controllers/logController.js
@@ -12,59 +12,6 @@ const addLog = async (req, res) => {
   }
 };
 
-// const searchLogs = async (req, res) => {
-//   try {
-//     const { query, filters, startDate, endDate } = req.body;
-
-//     const timestampFilter = {};
-//     if (startDate || endDate) {
-//       if (startDate) timestampFilter.$gte = new Date(startDate);
-//       if (endDate) timestampFilter.$lte = new Date(endDate);
-//     }
-
-//     const filterConditions = {
-//       timestamp: timestampFilter,
-//     };
-
-//     // Map filter fields to their corresponding model fields
-//     const filterFieldMap = {
-//       level: 'level',
-//       message: 'message',
-//       resourceId: 'resourceId',
-//       traceId: 'traceId',
-//       spanId: 'spanId',
-//       commit: 'commit',
-//       'metadata.parentResourceId': 'metadata.parentResourceId',
-//     };
-
-//     // Apply filters to filterConditions
-//     filters.forEach((filter) => {
-//       const { field, value } = filter;
-//       const modelField = filterFieldMap[field];
-//       if (modelField && value) {
-//         filterConditions[modelField] = new RegExp(value, 'i');
-//       }
-//     });
-
-//     const searchResults = await Log.find({
-//       $and: [
-//         {
-//           $or: [
-//             { message: new RegExp(query, 'i') },
-//             { level: new RegExp(query, 'i') },
-//           ],
-//         },
-//         filterConditions,
-//       ],
-//     });
-
-//     res.status(200).json(searchResults);
-//   } catch (error) {
-//     console.error('Error searching logs:', error.message);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// };
-
 const searchLogs = async (req, res) => {
   try {
     const { query, filters, startDate, endDate } = req.body;
